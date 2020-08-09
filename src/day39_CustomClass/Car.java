a00cf145685876a66563cd3b0b046d86f714679b
slokostic3212 <dsk3212@yahoo.com>
2020-08-09 11:02:02 -0400

    add day39_CustomClass

---
+++ src/day39_CustomClass/Car.java
@@ -0,0 +1,18 @@
+package day39_CustomClass;
+
+public class Car {
+    /*Attributes;
+        model brand mileage color year price
+     */
+
+    String brand;
+    String model;
+    int year;
+    String color;
+    double mileage;
+    double price;
+
+
+
+
+}
