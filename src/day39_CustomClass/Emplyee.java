88fdb9b6e69014f674aee0e849803454cc0d1b98
slokostic3212 <dsk3212@yahoo.com>
2020-08-09 19:08:01 -0400

    update day39_CustomClass

---
+++ src/day39_CustomClass/Emplyee.java
@@ -0,0 +1,28 @@
+package day39_CustomClass;
+
+public class Emplyee {
+/*
+attributes
+    employeeName gender SSN JobTitle salary
+ */
+
+    String name;
+    char gender;
+    String SSN;
+    String jobTitle;
+    double salary;
+
+    public void setEmployeeInfo(String employeeName, char employeeGender, String employeeSSN, String employeeJobTitle, double employeeSalary){
+        name = employeeName;
+        gender = employeeGender;
+        SSN = employeeSSN;
+        jobTitle = employeeJobTitle;
+        salary = employeeSalary;
+    }
+
+    public void getEmployeeInfo(){
+        System.out.println("Name: "+name+", Gender: "+gender+", JobTitle: "+jobTitle+", Salary: "+salary);
+        SSN = SSN.substring( SSN.length()-4 );
+        System.out.println("Last for digits of SSN: " +  SSN );
+    }
+}
