5d94bfb2bb1effe3860aca2c7321f27e851e9904
slokostic3212 <dsk3212@yahoo.com>
2020-08-09 12:17:54 -0400

    update day39_CustomClass

--- src/day39_CustomClass/Car.java
+++ src/day39_CustomClass/Car.java
@@ -12,6 +12,26 @@ public class Car {
     double mileage;
     double price;
 
+    public void seInfor(String carBrand, String carModel, int carYear, String carColor, double carMileage,
+                        double carPrice ){
+                                                // has static - not an instance method -
+                                                // attributes need instance variables
+                                                // actions/fxns need instance method
+                            // need static gone so to use instance to get 1 copy for each object
+        brand = carBrand;
+        model = carModel;
+        year = carYear;
+        color = carColor;
+        mileage = carMileage;
+        price = carPrice;
+
+
+
+
+    }
+/* car1 - Toyota
+    car2 - BMW
+ */
 
 
 
--- src/day39_CustomClass/CarObjects.java
+++ src/day39_CustomClass/CarObjects.java
@@ -1,15 +1,22 @@
 package day39_CustomClass;
 
+import com.sun.xml.internal.ws.addressing.WsaActionUtil;
+
 public class CarObjects {
 
     public static void main(String[] args) {
         Car car1 = new Car();
+
+        /*
             car1.brand = "Toyota";
             car1.model = "Corolla";
             car1.year = 2020;
             car1.color = "White";
             car1.mileage = 20000;
             car1.price = 19000.50;
+*/
+
+        car1.seInfor("Toyota", "Corolla", 2010, "Red", 55000, 16000);
 
         System.out.println(car1.brand);
         System.out.println(car1.model);
@@ -19,15 +26,20 @@ public class CarObjects {
 
         System.out.println("====================================");
         Car car2 = new Car();
+        /*
             car2.brand = "Mercedes";
             car2.model = "G 550";
             car2.year = 2000;
             car2.color = "Blue";
+        */
 
+        car2.seInfor( "BMW","X5", 2020,"Black", 1000, 60000 );
         System.out.println(car2.brand);
         System.out.println(car2.model);
         System.out.println(car2.year);
         System.out.println(car2.color);
+        System.out.println(car2.mileage);
+        System.out.println(car2.price);
 
 
 
