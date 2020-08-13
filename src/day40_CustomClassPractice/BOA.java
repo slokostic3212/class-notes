1e2b128dabb3cf3b7129116169c2fc251d9ad031
slokostic3212 <dsk3212@yahoo.com>
2020-08-12 20:07:00 -0400

    update day40_CustomClassPractice

--- src/day40_CustomClassPractice/BOA.java
+++ src/day40_CustomClassPractice/BOA.java
@@ -2,4 +2,29 @@ package day40_CustomClassPractice;
 
 public class BOA {
 
+    public static void main(String[] args) {
+
+        BankAccount Dawud = new BankAccount();
+        Dawud.setAccountInfo("Saving", "Dawud Abduwali", "123455786");
+
+        Dawud.getAccountInfo();
+
+        Dawud.deposit(1000);
+
+        Dawud.checkBalance();
+
+        Dawud.withDraw(500);
+
+        Dawud.checkBalance(); // 500
+
+        Dawud.withDraw(100000);
+
+        Dawud.checkBalance();
+
+
+    }
+
+
+
+
 }
--- src/day40_CustomClassPractice/BankAccount.java
+++ src/day40_CustomClassPractice/BankAccount.java
@@ -15,35 +15,43 @@ package day40_CustomClassPractice;
  */
 public class BankAccount {
 
-      String accountType;
-      String accountHolder;
-      String accountNumber;
-      double balance;
+    String accountType;
+    String accountHolder;
+    String accountNumber;
+    double balance;
 
-      public void setAccountInfo(String clientAccountType, String name, String acctNum){
-          accountType = clientAccountType;
-          accountHolder = name;
-          accountNumber = acctNum;
-      }
-      public void getAccountInfo(){
-          System.out.println("========================");
-          System.out.println("Account Holder: "+accountHolder);
-          System.out.println("Account Number: "+accountNumber);
-          System.out.println("Available Balance: "+balance);
-          System.out.println("=========================");
-      }
+    public void setAccountInfo(String clientAccountType, String name, String acctNum){ // sets the info
+        accountType = clientAccountType;
+        accountHolder = name;
+        accountNumber = acctNum;
+    }
 
-        public void checkBalance(){
-            System.out.println("Available Balance: "+balance);
+    public void getAccountInfo(){ // displays account info
+        System.out.println("============================");
+        System.out.println("Account Holder: "+accountHolder);
+        System.out.println("Account Number: "+accountNumber);
+        System.out.println("Available Balance: "+balance);
+        System.out.println("============================");
+    }
 
+    public void checkBalance(){
+        System.out.println("Available Balance: "+balance);
+    }
+
+    public void deposit(double amount){
+        System.out.println("Depositing $"+amount+" to "+accountNumber);
+        balance += amount;
+    }
+
+    public void withDraw(double amount){
+        if(amount > balance){
+            System.out.println("not enough balance");
+            return; //
         }
-        public void deposit(double amount){
-            System.out.println("Depositing $"+amount+" to "+accountNumber);
-          balance += amount;
-        }
-        public void withDraw(double amount){
-          System.out.println("Withdrawing $"+amount+" from "+accountNumber);
-          balance -= amount;
-        }
+
+        System.out.println("Withdrawing $" + amount + " from " + accountNumber);
+        balance -= amount;
 
     }
+
+}
