b906c4ff45442b1566759f0861eb2e35625719ee
slokostic3212 <dsk3212@yahoo.com>
2020-08-12 21:45:17 -0400

    update Offers.java

--- src/day40_CustomClassPractice/MyOffers.java
+++ src/day40_CustomClassPractice/MyOffers.java
@@ -1,5 +1,8 @@
 package day40_CustomClassPractice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class MyOffers {
     public static void main(String[] args) {
         Offer offer1 = new Offer();
@@ -10,6 +13,21 @@ public class MyOffers {
         Offer offer6 = new Offer();
         Offer offer7 = new Offer();
 
+        offer1.setOfferInfo(100000, "VA", true, true, true, "SDET", true);
+        offer2.setOfferInfo(175_000,"VA",true,true, false, "SDET", true);
+        offer3.setOfferInfo(125000,"VA", true, true, true, "SDET", true);
+        offer4.setOfferInfo(100_000,"PA",true,false, false, "QA", true);
+        offer5.setOfferInfo(100_000,"CA",true,false, false, "QA", true);
+        offer6.setOfferInfo(120000,"CO",true,true,true,"Senior SDET",true);
+        offer7.setOfferInfo(110_000,"GA",true,false, false, "QA", true);
+
+
+        ArrayList<Offer> jobOffers = new ArrayList<>();
+        jobOffers.addAll(Arrays.asList(offer1, offer2, offer3, offer4, offer5, offer6, offer7));
+
+        for ( Offer eachOffer : jobOffers){
+            eachOffer.getOfferInfo();
+        }
 
     }
 
