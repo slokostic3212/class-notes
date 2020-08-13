aca84075c2b8e9fbd68ef7735046bb30dd149a6f
slokostic3212 <dsk3212@yahoo.com>
2020-08-12 21:29:11 -0400

    update Offer.java

---
+++ src/day40_CustomClassPractice/MyOffes.java
@@ -0,0 +1,5 @@
+package day40_CustomClassPractice;
+
+public class MyOffes {
+
+}
--- src/day40_CustomClassPractice/Offer.java
+++ src/day40_CustomClassPractice/Offer.java
@@ -1,5 +1,25 @@
 package day40_CustomClassPractice;
+/*
+Attribute:
+			Salary, Location, hasPTO, isFulltime, WFH
 
+		Actions:
+		setOfferInfo
+		getOfferInfo
+
+		must use this keyword
+ */
 public class Offer {
+    double salary;
+    String state;
+    boolean hasPTO;
+    boolean isFullTime;
+    boolean isWFH;
+    String jobTitle;
+    boolean hasBenefit;
+
+
+
+
 
 }
