d618a1ec1bff4379f83f347f5cc731bba7111f61
slokostic3212 <dsk3212@yahoo.com>
2020-08-15 11:49:26 -0400

    update day41_Static

---
+++ src/day41_Static/CarpetObjects.java
@@ -0,0 +1,55 @@
+package day41_Static;
+/*
+ create a class called carpetObjects, and create an array of the carpet that contains 5 carpet objects ( make sure you set the instance variables of those objects)
+            create two ArrayList of carpets:
+
+                        persianCarpets
+                        reugularCarpets
+
+            write a program to store all the Persian Carpets into the list of persianCarpets, and store all regular carpets into the list of reugularCarpets
+                    use for each loop to print out all the Persian carpets
+
+ */
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.ArrayList;
+
+public class CarpetObjects {
+
+    public static void main(String[] args) {
+        Carpet[] carpets = { new Carpet(), new Carpet(), new Carpet(), new Carpet(), new Carpet() };
+                        //       0              1            2             3              4
+            carpets[0].customOrder(4.5, 3.5, 17.5, false);
+            carpets[1].customOrder(3.5, 3.5, 17.5, true);
+            carpets[2].customOrder(6.5, 3.5, 19.5, false);
+            carpets[3].customOrder(4.5, 3.5, 10.5, true);
+            carpets[4].customOrder(5.5, 3.5, 11.5, true);
+
+  /*      for ( Carpet each: carpets){
+            each.getCarpetInfo();
+
+   */
+        ArrayList<Carpet> persianCarpets = new ArrayList<>();
+        ArrayList<Carpet> regularCarpets = new ArrayList<>();
+/*
+        for (int i = 0; 1 <= carpets.length-1; i++){
+            if( carpets[1].isPersian){
+                persianCarpets.add( carpets[i]);
+            }else{
+                regularCarpets.add( carpets[i]);
+            }
+*/
+        for(Carpet each : carpets ){
+            if (each.isPersian){
+                persianCarpets.add(each);
+            }else{
+                regularCarpets.add(each);
+            }
+        }
+        System.out.println("number of persian carpets: "+persianCarpets.size());
+        System.out.println("numer of regual carpets: "+regularCarpets.size());
+        }
+
+    }
+
