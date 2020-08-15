e07c903a5874517abf625f99bb99f9ca5db20930
slokostic3212 <dsk3212@yahoo.com>
2020-08-15 16:56:52 -0400

    updat day41_toString

---
+++ src/day41_toString/Multi_Class.java
@@ -0,0 +1,26 @@
+package day41_toString;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Multi_Class {
+    public static void main(String[] args) {
+        System.out.println("class 1");
+    }
+
+
+}
+
+
+class test1{
+    public static void main(String[] args) {
+        System.out.println("class 2");
+    }
+
+}
+
+class test2{
+    public static void main(String[] args) {
+        System.out.println("class 3");
+    }
+
+}
