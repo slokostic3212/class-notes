160bc7f60a026ab3c5aef8370ee1010e703f321e
slokostic3212 <dsk3212@yahoo.com>
2020-08-19 19:02:39 -0400

    update Practice_08_19_2020

--- src/Office_Hours/Practice_08_19_2020/Pizza.java
+++ src/Office_Hours/Practice_08_19_2020/Pizza.java
@@ -19,26 +19,32 @@ public class Pizza {
     int numOfCheeseTopping;
     int numOfPepperoniTopping;
 
-    public void setPizzaInfo (String size, int numOfCheeseTopping, int numOfPepperoniTopping){
-        this.size = size;
+    public void setPizzaInfo(String size, int numOfCheeseTopping, int numOfPepperoniTopping){
+        this.size =  size;
         this.numOfCheeseTopping = numOfCheeseTopping;
         this.numOfPepperoniTopping = numOfPepperoniTopping;
-
     }
+
     public double calcPrice(){
-        int totalNumberOfToppimg = numOfCheeseTopping+ numOfPepperoniTopping;
-        double totalPriceOfTopping = totalNumberOfToppimg *2;
+        int totalNumberOfTopping = numOfCheeseTopping+ numOfPepperoniTopping;
+        double totalPriceOfTopping = totalNumberOfTopping *2;
         double totalPrice = totalPriceOfTopping;
-        if(size.equalsIgnoreCase( "small")){
+
+        if(size.equalsIgnoreCase("small")){
             totalPrice += 10;
-
-        }else if(  size.equalsIgnoreCase( "medium")){
-            totalPrice +=12:
+        }else if( size.equalsIgnoreCase("medium")){
+            totalPrice += 12;
         }else{
-            totalPrice +=14:
-
+            totalPrice += 14;
         }
+
         return totalPrice;
     }
 
-}
+    public String toString(){
+        return "Size: "+size+"\nNumber of Cheese Topping: "+numOfCheeseTopping
+                +"\nNumber of Pepperoni Topping: "+numOfPepperoniTopping
+                +"\n Total Price: "+calcPrice();
+        }
+    }
+
