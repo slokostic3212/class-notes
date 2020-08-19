035ae622f8416ccf6fb09b6591b10fc4b92d5f5e
slokostic3212 <dsk3212@yahoo.com>
2020-08-18 20:28:51 -0400

    update day43_Static

---
+++ src/day43_Static/CybertekStudent.java
@@ -0,0 +1,17 @@
+package day43_Static;
+
+class A{
+    static int a = 200;
+}
+
+public class CybertekStudent {
+
+    // for advantages of static
+
+    String studentName;
+    int age;
+    char gender;
+
+    static String schoolName = "Cybertek School";
+
+}
---
+++ src/day43_Static/studentObjects.java
@@ -0,0 +1,24 @@
+package day43_Static;
+
+import java.util.concurrent.Callable;
+
+public class studentObjects {
+
+    public static void main(String[] args) {
+
+        CybertekStudent obj1 = new CybertekStudent();
+
+        CybertekStudent obj2 = new CybertekStudent();
+
+        System.out.println( obj1.studentName);
+        System.out.println( obj2.studentName);
+
+        System.out.println(obj1.schoolName);
+        System.out.println(obj2.schoolName);
+
+
+    }
+
+}
+
+
