c87c743ab760c06662f4d5eb1cde5ab4121f6268
slokostic3212 <dsk3212@yahoo.com>
2020-08-18 21:59:08 -0400

    add Resources.java

---
+++ src/Libray/Resources.java
@@ -0,0 +1,18 @@
+package Libray;
+
+public class Resources {
+
+   public static int a = 200;
+    int b = 300;
+
+    public static void method1(){
+        System.out.println("static method");
+
+
+    }
+
+    public void method2(){
+        System.out.println("instance method");
+
+    }
+}
--- src/day43_Static/Test.java
+++ src/day43_Static/Test.java
@@ -2,6 +2,8 @@ package day43_Static;
 
 import java.util.Arrays;
 
+import static Library.Resources.a;// only imports static variable a
+
 public class Test {
     public static void main(String[] args) {
         System.out.println(staticBlock3.a);
@@ -10,6 +12,10 @@ public class Test {
 
         System.out.println(Arrays.toString( Data.arr));
         System.out.println(Data.list);
+
+        System.out.println("============================");
+
+        System.out.println(a);
 
     }
 }
