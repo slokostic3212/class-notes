d6939d5fc2952bd4540ac0999c1d86d934c6e1e2
slokostic3212 <dsk3212@yahoo.com>
2020-08-19 21:26:30 -0400

    update day44_Constructor

---
+++ src/day44_Constructor/ConstructorIntro.java
@@ -0,0 +1,25 @@
+package day44_Constructor;
+
+
+
+public class ConstructorIntro {
+
+    public ConstructorIntro(String arr) {
+        System.out.println("Hello "+arr);
+
+    }
+
+    public static void main(String[] args) {
+
+        //ConstructorIntro obj1 = new ConstructorIntro(10);
+
+       //ConstructorIntro obj2 = new ConstructorIntro();
+
+    ConstructorIntro obj3 =  new ConstructorIntro("Muhtar");
+
+
+
+
+    }
+
+}
