2d48e26d4aed2f6126793ddc449155d5de81c66f
slokostic3212 <dsk3212@yahoo.com>
2020-08-19 20:11:11 -0400

    updat day44_Constructor

--- src/day44_Constructor/HumanResource.java
+++ src/day44_Constructor/HumanResource.java
@@ -8,4 +8,26 @@ create a class called HumanResources
                         2. set all the info of the Employees
  */
 public class HumanResource {
+    static Employee employee1;
+    static Employee employee2;
+    static Employee employee3;
+    static Employee employee4;
+    static Employee employee5;
+
+   //employee1 = new Employee();
+
+    static{// as soon as static is called the static variables need to be set
+        employee1= new Employee();
+        employee2= new Employee();
+        employee3= new Employee();
+        employee4= new Employee();
+        employee5= new Employee();
+
+        employee1.setEmployeeInfo("Muhammed",12312,456432,"SDET",120000,'M');
+        employee2.setEmployeeInfo("Erkan",45678,123987,"QA",115000,'M');
+        employee3.setEmployeeInfo("Asiya",56431,657098,"Instructor",99000,'F');
+        employee4.setEmployeeInfo("Ayse",17876,342567,"SDET",125000,'F');
+        employee5.setEmployeeInfo("Ahmet",67543,987654,"Developer",140000,'M');
+
+    }
 }
