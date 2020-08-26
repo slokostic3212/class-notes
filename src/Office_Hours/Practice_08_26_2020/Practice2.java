8857c1723d97d1093c0a5dab5bb5562d03a7909f
slokostic3212 <dsk3212@yahoo.com>
2020-08-26 18:42:07 -0400

    update Practice_08_26_2020

--- src/Office_Hours/Practice_08_26_2020/Practice2.java
+++ src/Office_Hours/Practice_08_26_2020/Practice2.java
@@ -12,13 +12,42 @@ public class Practice2 {
         int[] arr3 = {100, 200, 300};
         printArray(arr3);
 
+
+        String[] arr4 = {"Ziiadin", "Aysen", "Murat", "Ramazan"};
+        printArray(arr4);
+
+        char[] arr5 = {'A', 'B', 'C', 'D', 'E'};
+        printArray(arr5);
+
     }
 
-    public static void printArray(int[] arr ){
-        for (int each : arr ){
-            System.out.print(each+" ");
 
+    public static void printArray(int[] arr) {
+        for (int each : arr) {
+            System.out.print(each + " ");
         }
         System.out.println();
     }
+
+    public static void printArray(String[] arr) {
+        for (String each : arr) {
+            System.out.print(each + " ");
+        }
+        System.out.println();
+    }
+
+    public static void printArray(char[] arr) {
+        for (char each : arr) {
+            System.out.print(each + " ");
+        }
+        System.out.println();
+    }
+
+    public static void printArray(double[] arr) {
+        for (double each : arr) {
+            System.out.print(each + " ");
+        }
+        System.out.println();
+    }
+
 }
