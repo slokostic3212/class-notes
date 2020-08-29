f61ac33ebd9973761446bc7004b90d92cc26ae39
slokostic3212 <dsk3212@yahoo.com>
2020-08-29 10:19:18 -0400

    add day47_Encapsulation

---
+++ src/day47_Encapsulation/AccessModifiers.java
@@ -0,0 +1,13 @@
+package day47_Encapsulations;
+
+public class AccessModifiers {
+
+    public static int publicVariable = 100;// 1 variable
+
+    public static void publicMethod(){
+        System.out.println("public method");// 1 method
+
+    }
+
+
+}
---
+++ src/day47_Encapsulation/Test.java
@@ -0,0 +1,10 @@
+package day47_Encapsulations;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println(AccessModifiers.publicVariable);
+        AccessModifiers.publicMethod();
+    }
+}
