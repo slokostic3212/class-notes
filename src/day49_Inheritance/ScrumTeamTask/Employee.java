b66ecc2d6af8d50922de9736c9fa53f9de42581b
slokostic3212 <dsk3212@yahoo.com>
2020-08-31 19:43:32 -0400

    add day49_Inheritance

---
+++ src/day49_Inheritance/ScrumTeamTask/Employee.java
@@ -0,0 +1,8 @@
+package day49_Inheritance.ScrumTeamTask;
+/*
+create a subclass of Person called Employee
+                attributes: name, age, gender, Salary, ID, jobTitle
+                methods: work, setEmployeeInfo
+ */
+public class Employee {
+}
