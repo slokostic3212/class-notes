371f2eb674ae222f571910867de65f6f9ae1e58d
slokostic3212 <dsk3212@yahoo.com>
2020-09-02 19:28:56 -0400

    update InheritanceTest.java

--- src/Library/InheritanceTest.java
+++ src/Library/InheritanceTest.java
@@ -1,4 +1,14 @@
 package Library;
 
-public class InheritanceTest {
+import day50_Inheritance_Overriding.AccessModifiers;
+
+public class InheritanceTest extends AccessModifiers {
+
+    public static void main(String[] args) {
+        System.out.println(InheritanceTest.publicData);// public is visible outside pkg
+        System.out.println(InheritanceTest.protectedData);// prot is visible to any child class anytime
+        //System.out.println(InheritanceTest.defaultData);// not visible outside pkg
+
+
+    }
 }
