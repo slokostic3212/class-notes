e065d32bdf9330e94a40f3fd12d7d19f33249fed
slokostic3212 <dsk3212@yahoo.com>
2020-09-02 21:58:02 -0400

    update day50_Inheritance_Overriding

---
+++ src/day50_Inheritance_Overriding/ShapeTask/ShapeObjects.java
@@ -0,0 +1,18 @@
+package day50_Inheritance_Overriding.ShapeTask;
+
+public class ShapeObjects {
+    public static void main(String[] args) {
+
+        Circle circle = new Circle(3);
+
+        System.out.println( circle.calculateArea());
+
+        Rectangle rectangle = new Rectangle(3,4);
+
+        System.out.println( rectangle.calculateArea() );//12
+
+        System.out.println( circle.calculatePerimeter() );
+
+        System.out.println(rectangle.calculatePerimeter());// this is a bug cuz u get 0
+    }
+}
