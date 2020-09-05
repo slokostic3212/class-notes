51d65999ce58c5dc704ffe2c93f427dcb90e630e
slokostic3212 <dsk3212@yahoo.com>
2020-09-05 10:48:56 -0400

    add day51_Exceptions

---
+++ src/day51_Exceptions/BrowsersTask/FireFoxBrowsers.java
@@ -0,0 +1,13 @@
+package day51_Exceptions.BrowsersTask;
+
+public class FireFoxBrowsers extends {
+    public void get(String url) {
+        System.out.println("Opening the " + url + " in firefox browser");
+
+    }
+
+    public void close() {
+        System.out.println("closing the firefox browser");
+    }
+
+}
