f1464b87b666307e7459470db309738dbd6ad30a
slokostic3212 <dsk3212@yahoo.com>
2020-09-08 21:56:34 -0400

    update day52_Exceptions

---
+++ src/day52_Exceptions/ThrowKeyword.java
@@ -0,0 +1,10 @@
+package day52_Exceptions;
+
+public class ThrowKeyword {
+
+    public static void main(String[] args) {
+
+        throw new ClassCastException();
+        //   System.out.println("Hello");
+    }
+}
---
+++ src/day52_Exceptions/ThrowPractice.java
@@ -0,0 +1,11 @@
+package day52_Exceptions;
+
+public class ThrowPractice {
+
+    public static void main(String[] args) {
+
+        String driverName = "chrome";
+
+
+    }
+}
