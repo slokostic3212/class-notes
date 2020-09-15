85d1a25c0b1b21f9b34c33acc33d083e5532533e
slokostic3212 <dsk3212@yahoo.com>
2020-09-15 19:37:27 -0400

    day54_Abstraction

---
+++ src/day54_Abstraction/Employee/Developer.java
@@ -0,0 +1,12 @@
+package day54_Abstraction.Employee;
+
+import java.time.LocalDate;
+
+public final class Developer extends Employee{
+
+           public Developer(String name, char gender, LocalDate DOfB, String jobTitle, double salary) {
+            super(name, gender, DOfB, jobTitle, salary);
+        }
+
+
+}
